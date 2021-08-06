c665cc1d02fc12007091837a86712427e0488aa4
IrvingC48 <c.ici.3015@hotmail.com>
2021-08-05 23:14:36 -0500

    add eventlisteners (change.products, keyup.amount)

--- sales/js/sales.js
+++ sales/js/sales.js
@@ -11,31 +11,30 @@ const OnLoad = () => {
 
 const templateSubtotal = (object, index) => {
     return `
-        <div class="prd-sub">
-            <div class="flex-row">
+        <div id=${index} class="prd-sub">
+            <div class="flex-column">
                 <label class="label-input lb-i-x">ID LISTA</label>
                 <label class="input100 lb-i-x">${Number(index)+1}</label>
             </div>
-            <div class="flex-row">
+            <div class="flex-column">
                 <label class="label-input lb-i-x">PRODUCTO</label>
                 <label class="input100 lb-i-x">${object.producto}</label>
             </div>
-            <div class="flex-row">
-                <label for="name" class="label-input lb-i-x">CANTIDAD</label>
-                <label for="name" class="input100 lb-i-x">${object.cantidad}</label>
+            <div class="flex-column">
+                <label class="label-input lb-i-x">CANTIDAD</label>
+                <label class="input100 lb-i-x">${object.cantidad}</label>
             </div>
-            <div class="flex-row">
-                <label for="name" class="label-input lb-i-x">SUBTOTAL</label>
-                <label for="name" class="input100 lb-i-x">${object.subtotal}</label>
+            <div class="flex-column">
+                <label class="label-input lb-i-x">SUBTOTAL</label>
+                <label class="input100 lb-i-x">${object.subtotal}</label>
             </div>
-            <div class="flex-row">
-                <button class="contact-sale-btn btn-mod" id="btn-test">MODIFICAR</button>
-                <button class="contact-sale-btn btn-del" id="btn-test">ELIMINAR</button>
+            <div class="flex-column">
+                <button class="contact-sale-btn btn-mod" id="btn-upd">MODIFICAR</button>
+                <button class="contact-sale-btn btn-del" id="btn-del">ELIMINAR</button>
             </div>
         </div>
     `;
 }
-
 
 document.getElementById("btn-addP").addEventListener('click', function(event) {
     event.preventDefault();
@@ -43,8 +42,10 @@ document.getElementById("btn-addP").addEventListener('click', function(event) {
     const nomC = document.getElementsByName("name")[0].value;
     const products = document.getElementById("products").value;
     const amount = document.getElementsByName("amount")[0].value;
-    const subtotal = document.getElementsByName("subtotal")[0].value;
+    const subtotal = document.getElementById("subtotal").innerText;
     const listSub = document.getElementById("ls-products");
+    const total = document.getElementsByName("total")[0];
+    let priceTotal = 0;
 
     addLS(products, amount, subtotal);
 
@@ -52,7 +53,9 @@ document.getElementById("btn-addP").addEventListener('click', function(event) {
     listSub.innerHTML = '';
     for (const key in listSales) {
         listSub.innerHTML += templateSubtotal(listSales[key], key);
+        priceTotal += Number(listSales[key].subtotal);
     }
+    total.innerText = priceTotal;
 
     // alert("Producto agregado");
 });
@@ -75,9 +78,51 @@ const addLS = (product, amount, subtotal) => {
 
 };
 
+document.getElementById("products").addEventListener("change", function() {
+    const objet_LS = JSON.parse(localStorage.getItem('datos'));
+    const valueAmount = document.getElementsByName("amount")[0].value;
+    const products = document.getElementById("products").value;
 
 
-const objeto = [{'nom_producto': 'Prd1', 'cnt_producto':10, 'exist_producto':true}, {'nom_producto': 'Prd2', 'cnt_producto':5, 'exist_producto':false}];
+    if (objet_LS !== null) {
+        const existProdL = objet_LS.filter((value) => (value.nom_producto === products))
+        if (existProdL.length > 0) {
+            sessionStorage.setItem('priceProduct', Number(existProdL[0].price));
+            calcSubtotal(Number(existProdL[0].price) , valueAmount);
+        } else {
+            sessionStorage.setItem('priceProduct', 0);
+            calcSubtotal(0 , valueAmount);
+        };
+    } else {
+        sessionStorage.setItem('priceProduct', 0);
+        calcSubtotal(0 , valueAmount);
+    };
+});
+
+document.getElementsByName("amount")[0].addEventListener("keyup", function() { 
+    const valueAmount = document.getElementsByName("amount")[0].value;
+    let valueProd = sessionStorage.getItem('priceProduct');
+
+    calcSubtotal(valueProd , valueAmount);
+});
+
+
+const calcSubtotal = (valueProd, valueAmount) => {
+    const subtotal = document.getElementById("subtotal");
+
+    if (valueProd === undefined) {valueProd = 0};
+    subtotal.innerText = valueProd * valueAmount;
+}
+
+// document.querySelectorAll()
+
+// document.getElementById("btn-upd").addEventListener('click', function(event) {
+//     event.parentNode
+
+//     // splice(index , 1);
+// };
+
+const objeto = [{'nom_producto': 'Prd1', 'cnt_producto':10, 'price':20, 'exist_producto':true}, {'nom_producto': 'Prd2', 'cnt_producto':5, 'price':12, 'exist_producto':false}];
 // console.log(objeto);
 document.getElementById("btn-test").addEventListener('click', function() {
     const listSub = document.getElementById("ls-products");
@@ -85,12 +130,11 @@ document.getElementById("btn-test").addEventListener('click', function() {
 
     // localStorage.setItem('datos', JSON.stringify(objeto));
 
-    // objet_LS.push({'nom_producto':'Prd3', 'cnt_producto':2, 'exist_producto':true}); //Método agregar producto
+    // let objet_LS = JSON.parse(localStorage.getItem('datos'));
+    // objet_LS.push({'nom_producto':'Prd3', 'cnt_producto':2, 'price':5, 'exist_producto':true}); //Método agregar producto
     // localStorage.setItem('datos', JSON.stringify(objet_LS));
+
     // localStorage.clear();
     sessionStorage.clear();
     alert("Venta Creada");
 });
-
-let objet_LS = JSON.parse(localStorage.getItem('datos'));
-console.log(objet_LS[0].nom_producto);
